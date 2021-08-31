d2bfb3d0d6e8f23a572993d826825540d4e3b463
yellowsunn <chohankook95@gmail.com>
2021-08-31 22:40:08 +0900

    이미지 웹캐시 추가

--- backend/src/main/java/com/yellowsunn/springblog/controller/ImageController.java
+++ backend/src/main/java/com/yellowsunn/springblog/controller/ImageController.java
@@ -1,17 +1,15 @@
 package com.yellowsunn.springblog.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Duration;
 
 @RestController
 public class ImageController {
@@ -19,15 +17,30 @@ public class ImageController {
     @Value("${file.upload.directory}")
     private String uploadPath;
 
-    @GetMapping(value = "/image/{imageName}", produces = MediaType.IMAGE_PNG_VALUE)
+    @GetMapping(value = "/image/{imageName}")
     public ResponseEntity<byte[]> showImage(@PathVariable("imageName") String imageName) throws IOException {
         try {
             FileInputStream fis = new FileInputStream(uploadPath + imageName);
             byte[] bytes = fis.readAllBytes();
             fis.close();
-            return new ResponseEntity<>(bytes, HttpStatus.OK);
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(Duration.ofDays(7L)))
+                    .contentType(getImageMediaType(imageName))
+                    .body(bytes);
         } catch (FileNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private MediaType getImageMediaType(String imageName) {
+        int pos = imageName.lastIndexOf('.');
+        String imageType = imageName.substring(pos + 1);
+        if ("gif".equalsIgnoreCase(imageType)) {
+            return MediaType.IMAGE_GIF;
+        } else if ("png".equalsIgnoreCase(imageType)) {
+            return MediaType.IMAGE_PNG;
+        } else {
+            return MediaType.IMAGE_JPEG;
         }
     }
 }
